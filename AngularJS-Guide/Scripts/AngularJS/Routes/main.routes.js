9da0b56d6b1e9ff99e1a42407543916bb12b7efe
Xavier Er <xavier_er@live.com.sg>
2015-09-27 20:58:22 +0800

    Add main.routes.js

---
+++ AngularJS-Guide/Scripts/AngularJS/Routes/main.routes.js
@@ -0,0 +1,17 @@
+﻿(function (angular) {
+    'use strict';
+
+    MainRoutes.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    angular
+        .module('angularJSGuideApp')
+        .config(MainRoutes);
+
+    function MainRoutes($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'Templates/Home/Index.html'
+            });
+    }
+})(angular);
