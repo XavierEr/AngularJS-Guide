4ffbf3139f21b52a7342dcf60137a3e15d3491a1
Xavier Er <xavier_er@live.com.sg>
2015-11-06 23:45:37 +0800

    Add FileUpload

---
+++ AngularJS-Guide/Scripts/AngularJS/Controllers/fileUpload.controller.js
@@ -0,0 +1,11 @@
+﻿(function (angular) {
+    ServerSideTableController.$inject = ['$scope', '$http'];
+
+    angular
+        .module('angularJSGuideApp')
+        .controller('FileUploadController', FileUploadController);
+
+    function FileUploadController($scope, $http) {
+
+    }
+})(angular);
---
+++ AngularJS-Guide/Scripts/AngularJS/Routes/fileupload.routes.js
@@ -0,0 +1,19 @@
+﻿(function (angular) {
+    'use strict';
+
+    MainRoutes.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
+
+    angular
+        .module('angularJSGuideApp')
+        .config(MainRoutes);
+
+    function MainRoutes($locationProvider, $stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('fileupload', {
+                url: '/fileupload',
+                templateUrl: '/Templates/FileUpload/FileUpload.html',
+                controller: 'FileUploadController'
+            });;
+    }
+})(angular);
