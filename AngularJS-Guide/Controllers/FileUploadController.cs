5811e182a4dbf6b36bd1f72e420185b23ca8582f
Xavier Er <xavier_er@live.com.sg>
2015-11-13 18:39:11 +0800

    Add csvhelper

--- AngularJS-Guide/Controllers/FileUploadController.cs
+++ AngularJS-Guide/Controllers/FileUploadController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -13,20 +16,28 @@ namespace AngularJS_Guide.Controllers
         [HttpPost]
         public async Task<IHttpActionResult> Upload()
         {
-            if (!Request.Content.IsMimeMultipartContent())
+            if (!this.Request.Content.IsMimeMultipartContent())
             {
                 throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
             }
 
             var provider = new MultipartMemoryStreamProvider();
-            await Request.Content.ReadAsMultipartAsync(provider);
+            await this.Request.Content.ReadAsMultipartAsync(provider);
             foreach (var file in provider.Contents)
             {
                 var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
+                var extension = Path.GetExtension(filename);
                 var buffer = await file.ReadAsByteArrayAsync();
+                var stream = await file.ReadAsStreamAsync();
+
+                using (var streamReader = new StreamReader(stream))
+                {
+                    var csvReader = new CsvReader(streamReader);
+                    csvReader.getre
+                }
+                
                 //Do whatever you want with filename and its binaray data.
             }
-
             return Ok();
         }
     }
