9255d82012bb43d7f7acb47e13f08cd1ba0571c9
Xavier Er <xavier_er@live.com.sg>
2015-11-16 15:46:20 +0800

    Add cache extensions and csv reader

---
+++ AngularJS-Guide/Common/CacheExtensions.cs
@@ -0,0 +1,36 @@
+﻿using AngularJS_Guide.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+
+namespace AngularJS_Guide.Common
+{
+    public static class CacheExtensions
+    {
+        private const string CUSTOMER_INFOS_CACHE_KEY = "customerInfos";
+
+        public static void SetCustomerInfos(this ObjectCache cache, IEnumerable<CustomerInfo> customerInfos)
+        {
+            if (customerInfos == null)
+            {
+                throw new ArgumentNullException("customerInfos");
+            }
+
+            if (cache[CUSTOMER_INFOS_CACHE_KEY] != null)
+            {
+                cache.Remove(CUSTOMER_INFOS_CACHE_KEY);
+            }
+            cache.Set(CUSTOMER_INFOS_CACHE_KEY, customerInfos, new CacheItemPolicy() { AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(5) });
+        }
+
+        public static IEnumerable<CustomerInfo> GetCustomerInfos(this ObjectCache cache)
+        {
+            if (cache[CUSTOMER_INFOS_CACHE_KEY] != null)
+            {
+                return cache[CUSTOMER_INFOS_CACHE_KEY] as IEnumerable<CustomerInfo>;
+            }
+            return Enumerable.Empty<CustomerInfo>();
+        }
+    }
+}
---
+++ AngularJS-Guide/Common/Csv.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AngularJS_Guide.Common
+{
+    public static class Csv
+    {
+        public static IEnumerable<T> Read<T>(Stream stream, Encoding encoding)
+        {
+            using (var streamReader = new StreamReader(stream, encoding))
+            {
+                var csvReader = new CsvReader(streamReader);
+
+                return csvReader.GetRecords<T>().ToArray();
+            }
+        }
+    }
+}
