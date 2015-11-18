716b6e6cf1a7afb950d76b28fd4f249bdc38b6bb
Xavier Er <xavier_er@live.com.sg>
2015-11-18 17:26:53 +0800

    Add register and update all references version

---
+++ AngularJS-Guide/Scripts/AngularJS/Controllers/register.controller.js
@@ -0,0 +1,20 @@
+﻿(function (angular) {
+    RegisterController.$inject = ['$scope', '$http'];
+
+    angular
+        .module('angularJSGuideApp')
+        .controller('RegisterController', RegisterController);
+
+    function RegisterController($scope, $http) {
+        $scope.email;
+        $scope.password;
+        $scope.confirmPassword;
+
+        $scope.register = function () {
+            $http.post('api/Account/Register', { email: $scope.email, password: $scope.password, confirmPassword: $scope.confirmPassword })
+            .then(function successCallback(response) {
+            }, function errorCallback(response) {
+            });
+        };
+    }
+})(angular);
---
+++ AngularJS-Guide/Scripts/AngularJS/Routes/account.routes.js
@@ -0,0 +1,19 @@
+﻿(function (angular) {
+    'use strict';
+
+    AccountRoutes.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
+
+    angular
+        .module('angularJSGuideApp')
+        .config(AccountRoutes);
+
+    function AccountRoutes($locationProvider, $stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('register', {
+                url: '/register',
+                templateUrl: '/Templates/Account/Register.html',
+                controller: 'RegisterController'
+            });
+    }
+})(angular);
