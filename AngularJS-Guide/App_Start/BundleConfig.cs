541018984daf89e2d68bbd618fe8ad594c8dd42b
Xavier Er <xavier_er@live.com.sg>
2015-11-18 12:11:21 +0800

    Add jquery ui reference

--- AngularJS-Guide/App_Start/BundleConfig.cs
+++ AngularJS-Guide/App_Start/BundleConfig.cs
@@ -30,6 +30,9 @@ namespace AngularJS_Guide
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
